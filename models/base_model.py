75360a58652a0f065fefd7ddbb0e2c474b70add8
johnlivingprooff <johnlivingprooff@gmail.com>
2023-12-04 18:21:15 +0200

    Added kwargs, from dict representation

--- models/base_model.py
+++ models/base_model.py
@@ -10,11 +10,19 @@ class BaseModel:
     other classes
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initializes once an instance is created"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
+        else:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key == 'created_at' or key == 'updated_at':
+                        self.__dict__[key] = datetime.fromisoformat(value)
+                    else:
+                        self.__dict__[key] = value
 
     def __str__(self):
         """Returns readable string representation"""
