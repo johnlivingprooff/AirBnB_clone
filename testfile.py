1496028a9d1810087d811ad593cacc2344cc85ca
johnlivingprooff <johnlivingprooff@gmail.com>
2023-12-06 17:21:43 +0200

    removed testfile

--- testfile.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-from models.base_model import BaseModel
-
-a = BaseModel({'my_number': 89, 'name': 'My First Model', 'id': 'b6a6e15c-c67d-4312-9a75-9d084935e579'})
-
-inst = BaseModel()
-
-print(inst)
